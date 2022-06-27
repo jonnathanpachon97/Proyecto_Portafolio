16d5fe5f5833ff3a20ba65cad99427f2192faa47
jonnathan nicolas pachon paiva <jonnathanpachon10@gmail.com>
2022-06-27 16:31:47 -0500

    subiendo proyecto de portafolio con html5 ccs3 y javascript

---
+++ index.js
@@ -0,0 +1,62 @@
+
+/*
+* 1. FUNCIONES MENU PRINCIPAL
+*/
+
+function hojaDeVida(){
+    location.href = "https://jonnathanpachon97.github.io/Taller_JavaScript_hojadevida/index.html";
+}
+
+function pseint(){
+    location.href = "/html/pseint.html";
+}
+
+function java(){
+    location.href = "/html/java.html";
+}
+
+
+/*
+* 2. FUNCIONES MENU PSEINT
+*/
+
+function variablesEImpresion(){
+    location.href = "/html/variableseimpresion.html";
+}
+
+function condicionalespseint(){
+    location.href = "/html/condicionalespseint.html";
+}
+
+function ciclospseint(){
+    location.href = "/html/ciclospseint.html";
+}
+
+function arreglospseint(){
+    location.href = "/html/arreglospseint.html";
+}
+
+function funcionespseint(){
+    location.href = "/html/funcionespseint.html";
+}
+
+
+/*
+* 3. FUNCIONES MENU JAVA
+*/
+
+function variablesEImpresionJava(){
+    location.href = "/html/variableseimpresionjava.html";
+}
+
+function condicionalesJava(){
+    location.href = "/html/condicionalesjava.html";
+}
+
+function ciclosJava(){
+    location.href = "/html/ciclosjava.html";
+}
+
+function arreglosJava(){
+    location.href = "/html/arreglosjava.html";
+}
